2ac2d5fa194bcdf207248983a214ee493ab7c0f3
default73 <78472408+default73@users.noreply.github.com>
2021-04-15 18:44:09 +0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ function validate() {
 		for (var j = 0; j < 4; j++){
 			var id = "a" + i + j;
 			n = document.getElementById(id).value;
-			if (n == 0 || n == 1){
+			if ((n == 0 || n == 1) && (n != "" || error == 1)){
 				arr[i][j] = n;
 			}
 			else{
@@ -20,7 +20,24 @@ function validate() {
 		}
 	}
 	if (error == 0){
-		
+		var res = 0;
+		for (var i = 0; i < 4; i++){
+			var one = 0;
+			for (var j = 0; j < 4; j++){
+				if (arr[i][j] == 1){
+					one += 1;
+				}
+			}
+			if (one == 1){
+				res += 1;
+			}
+		}
+		if (res == 4){
+			alert("Отношение является функцией");
+		}
+		else{
+			alert("Отношение НЕ является функцией");
+		}
 	}
 	console.log(arr);
 }
